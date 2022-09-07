66f907259a218a80e43a355c7e0074bb83c0853c
Ivan Sabo <123sabo@gmail.com>
2022-09-07 11:39:17 +0200

    log implementation, log tests - wip

---
+++ internal/log/log_test.go
@@ -0,0 +1,43 @@
+package log
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	api "github.com/123sabo/distributed-services/api/v1"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLog(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T, log *Log,
+	){
+		"append and read a record succeeds": testAppendRead,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			dir, err := ioutil.TempDir("", "store-test")
+			require.NoError(t, err)
+			defer os.RemoveAll(dir)
+			c := Config{}
+			c.Segment.MaxStoreBytes = 32
+			log, err := NewLog(dir, c)
+			require.NoError(t, err)
+
+			fn(t, log)
+		})
+	}
+}
+
+func testAppendRead(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	off, err := log.Append(append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, append.Value, read.Value)
+}
